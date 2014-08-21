e12031b891ddb738b34fb73296b00d46d88c64c8
juwang <juwang@microstrategy.com>
2014-08-21 13:35:16 +0800

    add free memory part

--- linkedListQuickSort.cpp
+++ linkedListQuickSort.cpp
@@ -2,10 +2,11 @@
 
 int main()
 {
-    MyTestSpace::LinkedList<int>* pList = MyTestSpace::GenerateRandomLinkedList<int>(10);
+    MyTestSpace::LinkedList<int>* pList = MyTestSpace::GenerateRandomLinkedList<int>(50);
     MyTestSpace::PrintLinkedList(pList);
     MyTestSpace::LinkedList<int>* pAfter = MyTestSpace::QuickSort(pList);    
     std::cout << std::endl;
     MyTestSpace::PrintLinkedList(pAfter);
+    delete pAfter;
     return 0; 
 }
